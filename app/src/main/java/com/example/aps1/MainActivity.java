6dec7eadff90255a958f7b11f5281d509d98f452
BarShwarts <sbar1998@gmail.com>
2024-05-22 01:20:23 +0300

    my first commit

--- app/src/main/java/com/example/aps1/MainActivity.java
+++ app/src/main/java/com/example/aps1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.aps1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView(this);
+        textView.setText("Hello World!");
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.addView(textView);
+        setContentView(linearLayout);
     }
 }
